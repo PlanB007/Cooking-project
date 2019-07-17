d8fa0ad5f1b52ea2b521ae503fb4bb0f476b8b6b
Kevin Rouwkema <kevin@about-blank.nl>
2019-07-17 15:36:33 +0200

    Created logging services

--- src/app/feature/recipe-book/list/list.component.ts
+++ src/app/feature/recipe-book/list/list.component.ts
@@ -1,3 +1,4 @@
+import { LoggingService } from './../../../services/logging/logging.services';
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
@@ -5,22 +6,25 @@ import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
+  providers: [LoggingService]
 })
 export class ListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
+  constructor(private logginsSerive: LoggingService) {}
+
   recipes: Recipe[] = [
     new Recipe('A Test recipe', 'This is simply a test', 'https://storage.needpix.com/rsynced_images/recipe-575434_1280.png' ),
     new Recipe('A Second recipe', 'This is a test', 'https://upload.wikimedia.org/wikipedia/commons/0/0b/Recipe_Unlimited_logo.png' )
   ];
-
-  constructor() { }
 
   ngOnInit() {
   }
 
   onRecipeSelected(recipe: Recipe) {
     this.recipeWasSelected.emit(recipe);
+    this.logginsSerive.logToConsole('Recipe selected');
   }
 
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,8 +7,8 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   @Output() featureSelected = new EventEmitter<string>();
-  
-  onSelect(feature: string){
+
+  onSelect(feature: string) {
     this.featureSelected.emit(feature);
   }
 
